684592b4e8d3e128f258a007ef8ade98313f36a5
pradnyesh45 <pradnyeshaglawe12@gmail.com>
2021-01-01 15:33:30 +0530

    Buttons

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 import HomeScreen from "./src/screens/HomeScreen";
-import ComponenetsScreen from './src/screens/ComponentsScreen';
-import ListScreen from './src/screens/ListScreen';
+import ComponenetsScreen from "./src/screens/ComponentsScreen";
+import ListScreen from "./src/screens/ListScreen";
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
     Components: ComponenetsScreen,
-    List: ListScreen
+    List: ListScreen,
   },
   {
-    initialRouteName: "List",
+    initialRouteName: "Home",
     defaultNavigationOptions: {
-      title: "App"
-    }
+      title: "App",
+    },
   }
 );
 
--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,27 +3,36 @@ import { View, Text, StyleSheet, FlatList } from "react-native";
 
 const ListScreen = () => {
   const friends = [
-    { name: "Firend #1" },
-    { name: "Firend #2" },
-    { name: "Firend #3" },
-    { name: "Firend #4" },
-    { name: "Firend #5" },
-    { name: "Firend #6" },
-    { name: "Firend #7" },
-    { name: "Firend #8" },
-    { name: "Firend #9" },
+    { name: "Firend #1", age: 20 },
+    { name: "Firend #2", age: 45 },
+    { name: "Firend #3", age: 32 },
+    { name: "Firend #4", age: 27 },
+    { name: "Firend #5", age: 53 },
+    { name: "Firend #6", age: 55 },
+    { name: "Firend #7", age: 30 },
+    { name: "Firend #8", age: 48 },
+    { name: "Firend #9", age: 76 },
   ];
 
   return (
     <FlatList
+      keyExtractor={(friend) => friend.name}
       data={friends}
       renderItem={({ item }) => {
-        return <Text>{item.name}</Text>;
+        return (
+          <Text style={styles.textStyle}>
+            {item.name} - Age {item.age}
+          </Text>
+        );
       }}
     />
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  textStyle: {
+    marginVertical: 50,
+  },
+});
 
 export default ListScreen;

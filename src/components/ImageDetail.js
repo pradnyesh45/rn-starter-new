b81dbe45b391af730ff46fa93eb4e36bcef46fc1
pradnyesh45 <pradnyeshaglawe12@gmail.com>
2021-01-06 15:34:26 +0530

    Images Download

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
 const ImageDetail = (props) => {
-  return <Text>{props.title}</Text>;
+  return (
+    <View>
+      <Image source={} />
+      <Text>{props.title}</Text>
+    </View>
+  );
 };
-
 const style = StyleSheet.create({});
 
 export default ImageDetail;

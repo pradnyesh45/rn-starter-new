2942ce67e6726006f26daa89378a2e239162e8bc
pradnyesh45 <pradnyeshaglawe12@gmail.com>
2021-01-28 10:17:19 +0530

    Exercise Outline

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,7 +1,12 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
+const COUNT = 1;
+
+const reducer = (state, action) => {};
+
 const CounterScreen = () => {
+  const [state, dispatch] = useReducer(reducer, { counter: 0 });
   const [counter, setCounter] = useState(0);
 
   return (

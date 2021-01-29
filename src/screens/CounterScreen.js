34e513b3ce1c6aae0eb19b0eb47d9c844c2b7905
pradnyesh45 <pradnyeshaglawe12@gmail.com>
2021-01-29 11:00:55 +0530

    Two important Props

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,31 +1,37 @@
 import React, { useReducer } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
-const COUNT = 1;
-
-const reducer = (state, action) => {};
+const reducer = (state, action) => {
+  // state === {count: number}
+  // action === {type: 'increment' || decrement, payload: 1}
+  switch (action.type) {
+    case "increment":
+      return { ...state, count: state.count + action.payload };
+    case "decrement":
+      return { ...state, count: state.count - action.payload };
+    default:
+      return state;
+  }
+};
 
 const CounterScreen = () => {
-  const [state, dispatch] = useReducer(reducer, { counter: 0 });
-  const [counter, setCounter] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
 
   return (
     <View>
       <Button
         title="Increase"
         onPress={() => {
-          // Don't do this;
-          // counter++;
-          setCounter(counter + 1);
+          dispatch({ type: "increment", payload: 1 });
         }}
       />
       <Button
         title="Decrease"
         onPress={() => {
-          setCounter(counter - 1);
+          dispatch({ type: "decrement", payload: 1 });
         }}
       />
-      <Text> Current Count: {counter} </Text>
+      <Text> Current Count: {state.count} </Text>
     </View>
   );
 };

b63487ccb3974a99c3e16bded392f1ca86d80be9
pradnyesh45 <pradnyeshaglawe12@gmail.com>
2021-02-01 10:40:44 +0530

    State Management in React Components

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -3,14 +3,31 @@ import { View, Text, StyleSheet, TextInput } from "react-native";
 
 const TextScreen = () => {
   const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <View>
+      {/* <Text>Enter Name:</Text>
       <TextInput
         style={styles.input}
         autoCapitalize="none"
         autoCorrect={false}
-        value="hi there"
+        value={name}
+        onChangeText={(newValue) => setName(newValue)}
       />
+      <Text>My name is {name}</Text> */}
+
+      <Text>Enter Password:</Text>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={password}
+        onChangeText={(newValue) => setPassword(newValue)}
+      />
+      {password.length < 5 ? (
+        <Text>Password must be longer than 5 characters</Text>
+      ) : null}
     </View>
   );
 };

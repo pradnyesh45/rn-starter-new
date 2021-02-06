359e6c34952712944d5c1dc08d4b25e8db124f06
pradnyesh45 <pradnyeshaglawe12@gmail.com>
2021-02-06 10:40:32 +0530

    Justify Content

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,9 +4,9 @@ import { Text, View, StyleSheet } from "react-native";
 const BoxScreen = () => {
   return (
     <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>Box Screen</Text>
-      <Text style={styles.textStyle}>Box Screen</Text>
-      <Text style={styles.textStyle}>Box Screen</Text>
+      <Text style={styles.textOneStyle}>Child #1</Text>
+      <Text style={styles.textTwoStyle}>Child #2</Text>
+      <Text style={styles.textThreeStyle}>Child #3</Text>
     </View>
   );
 };
@@ -15,11 +15,21 @@ const styles = StyleSheet.create({
   viewStyle: {
     borderWidth: 3,
     borderColor: "black",
+    height: 200,
+    flexDirection: "row",
+    justifyContent: "flex-start",
   },
-  textStyle: {
-    borderWidth: 10,
+  textOneStyle: {
+    borderWidth: 3,
     borderColor: "red",
-    margin: 20,
+  },
+  textTwoStyle: {
+    borderWidth: 3,
+    borderColor: "red",
+  },
+  textThreeStyle: {
+    borderWidth: 3,
+    borderColor: "red",
   },
 });
 
